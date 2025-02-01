aeb7e223d01672f38eaf4c41072360eda1c02869
am205379@gmail.com <am205379@gmail.com>
2025-02-01 15:54:05 +0330

    Implement FILL_PIXEL Macro

--- APaint.asm
+++ APaint.asm
@@ -4,24 +4,43 @@
 
 ;General Marco Section
 ;-------------------------------
+CLEAR_SCREEN  MACRO 
+        MOV   AH, 07H         
+        MOV   DH, 25          
+        MOV   DL, 40          
+        MOV   AL, 00H         
+        INT   10H   
+ENDM
+
 DISPLAY_MESSAGE MACRO   MESSAGE
-        MOV     DX, OFFSET MESSAGE
-        MOV     AH, 09H
-        INT     21H
+        MOV   DX, OFFSET MESSAGE
+        MOV   AH, 09H
+        INT   21H
 ENDM
 
 SET_CURSOR  MACRO   ROW COL
-        MOV     AH, 02H
-        MOV     BH, 0   
-        MOV     DH, ROW 
-        MOV     DL, COL 
-        INT     10H     
+        MOV   AH, 02H
+        MOV   BH, 0   
+        MOV   DH, ROW 
+        MOV   DL, COL 
+        INT   10H     
 ENDM
 
-CLEAR_SCREEN    MACRO 
-        MOV     AH, 07H         
-        MOV     DH, 25          
-        MOV     DL, 40          
-        MOV     AL, 00H         
-        INT     10H   
+FILL_PIXEL  MACRO   COLOR
+        MOV   AL, COLOR
+        MOV   AH, 0CH
+        INT   10H
+ENDM
+
+DRAW_COLOR_BOX  MACRO   COLOR, START_ROW, END_ROW
+        LOCAL   ROW_LOOP, COL_LOOP
+        MOV     DX, START_ROW
+    ROW_LOOP:
+        MOV     CX, 16           
+    COL_LOOP:
+        FILL_PIXEL  COLOR                         
+        LOOP      COL_LOOP       
+        INC     DX             
+        CMP     DX, END_ROW     
+        JB      ROW_LOOP        
 ENDM
